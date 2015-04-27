febe1ecc6ba58ae11a55ad3a4c5268ebade5d718
Aaron Covington <TheAaronCovington@gmail.com>
2015-04-27 18:11:39 -0500

    Create Array Manipulations.java
    Useful array methods I've written to insert, delete, and allocate more space (by creating a new array) if needed.

---
+++ Array Manipulations.java
@@ -0,0 +1,38 @@
+/*
+ * This file will hold array manipulation functions.
+ */
+public class ArrayManip {
+	public static int nElems;
+	
+	public static int[] insert(int[] myArray, int index, int number) {
+		nElems = myArray.length - 1;
+		if (nElems + 1 == myArray.length) { //if adding an element runs you out of space
+			myArray = allocate(myArray); //allocate more space in array
+		}
+		
+		for (int i = nElems; i >= index; i--) {
+			myArray[i+1] = myArray[i]; //shift elements to the right
+		}
+		myArray[index] = number; //insert new value
+		nElems++; //increment count of elements
+		return myArray;
+	}
+	
+	public static int[] delete(int[] myArray, int index) {
+		nElems = myArray.length - 1;
+		for (int i = index; i < nElems; i++) {
+			myArray[i] = myArray[i+1]; //shift elements left to delete
+		}
+		return myArray;
+	}
+	
+	//This method will allocate a new array with twice as much space
+	public static int[] allocate(int[] myArray){
+		int[] myArrayb = new int[2 * myArray.length];
+		for (int i = 0; i <= nElems; i++) {
+			myArrayb[i] = myArray[i]; 
+		}
+		myArray = myArrayb;
+		return myArray;
+	}
+}
