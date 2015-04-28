c3241b1f20d72ff6c91278f108cc3f8b7f282414
Aaron Covington <TheAaronCovington@gmail.com>
2015-04-28 05:58:37 -0500

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,50 +1,55 @@
-
+/*
+ * This is a simple implementation of a Linked List data structure.  It currently has the
+ * ability to add, delete, find, and print the items in the list.
+ */
 public class MyLinkedList {
 	
 	Node head, tail;
 	int size = 0;
 
 	public void add(int data) {
-		Node myNode = new Node(data);
+		Node newNode = new Node(data);
 		
-		if (tail == null) {
-			head = myNode;
-			tail = myNode;
+		if (tail == null) {  //checks if the list is empty
+			head = newNode; //if list is empty the new node will be both the head and tail
+			tail = newNode;
 		}
-		else {
-			tail.nextNode = myNode;
-			tail = myNode;
+		else { //if the list isn't empty
+			tail.nextNode = newNode;
+			tail = newNode;  //set tail = to the new node
 		}
-		size++;
+		size++;  //increment size to track number of items in the list
 	}
 	
 	public Node delete(int data) {
 		Node nodeToReturn = null;
-		if (size == 0) {
+		if (size == 0) { //checks if list is empty
+			System.out.println("List is empty!");
 			return null;
 		}
 		if (size == 1) {
 			nodeToReturn = head;
-			head = null;
-			tail = null;
-			size--;
+			head = null; //deletes the only item in list by setting head and tail to null
+			tail = null; 
+			size--; //decrement count of items in the list
 			return nodeToReturn;
 		}
 		
+		//get the node before the one we want to delete
 		Node nodeBeforeNodeToDelete = findNodeBefore(data);
 		//case where we need to delete the head
-		if (nodeBeforeNodeToDelete.data == 0) {
+		if (nodeBeforeNodeToDelete.data == 0) { 
 			head = head.nextNode;
 		}
-		else if (nodeBeforeNodeToDelete!= null) {
-			if (tail.data == data) { //handles last item
-				nodeBeforeNodeToDelete.nextNode = null;
-				tail = nodeBeforeNodeToDelete;
+		else if (nodeBeforeNodeToDelete!= null) { //runs if a data match was found
+			if (tail.data == data) { //if data in the tail matches
+				nodeBeforeNodeToDelete.nextNode = null; //deletes the node
+				tail = nodeBeforeNodeToDelete; //sets tail to node before the one deleted
 			}
-			else {
+			else { //if the data doesn't match, go to node after next node
 				nodeBeforeNodeToDelete.nextNode = nodeBeforeNodeToDelete.nextNode.nextNode;
 			}
-			size--;
+			size--; //decrement count after deletion
 		}
 
 		return null;
@@ -60,44 +65,48 @@ public class MyLinkedList {
 		
 		//iterate through our linked list
 		while (node.nextNode != null) {
-			if(node.nextNode.data == data) {
-				return node;
+			if(node.nextNode.data == data) { //checks if next nodes data matches search key
+				return node; //if match is found, return the node before node containing data.
 			}
-			node = node.nextNode;
+			node = node.nextNode; //load the next node
 		}
-		
+		System.out.println(data + " was not found!"); //if no match is found in the list
 		return null;
 	}
 	
 	public Node find(int data) {
-		if (head == null) {
+		if (head == null) { //Checks if list is empty
+			System.out.println("List is empty!");
 			return null;
 		}
-		if (head.data == data) {
+		if (head.data == data) { //if data found in first node
 			System.out.println(data + " was found!");
 			return head;
 		}
 		Node myNode = head;
 		
-		while (myNode.nextNode != null) {
-			myNode = myNode.nextNode;
-			if (myNode.data == data) {
-				System.out.println(data + " was found!");
+		while (myNode.nextNode != null) { //loop through the entire list
+			myNode = myNode.nextNode; //load the next node
+			if (myNode.data == data) { //check to see if it matches search key
+				System.out.println(data + " was found!"); //if it does print results
 				return myNode;
 			}
 		}
-		System.out.println(data + " was not found!");
+		System.out.println(data + " was not found!"); //if no match found in the list
 		return null;
 	}
 	
 	public void traverse() {
-		if (head != null) {
+		if (head != null) { //checks if list is empty
 			Node node = head;
-			System.out.print(node.data + " ");
-			while (node.nextNode != null) {
-				node = node.nextNode;
-				System.out.print(node.data + " ");
+			System.out.print(node.data + " ");  //prints first item
+			while (node.nextNode != null) {  //loops through remaining items if they exist
+				node = node.nextNode;  //sets node to the next node on each pass
+				System.out.print(node.data + " ");  //prints the node
 			}
+		}
+		else {
+			System.out.println("List is empty!");
 		}
 	}
 
