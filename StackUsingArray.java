66ad7c0aec51d5ad71f8a79d7993955a5da98874
Aaron Covington <TheAaronCovington@gmail.com>
2015-05-09 23:49:12 -0500

    Update StackUsingArray.java
    Reworked code to make it use generics

--- StackUsingArray.java
+++ StackUsingArray.java
@@ -1,44 +1,66 @@
+import java.util.NoSuchElementException;
+
 /*
  * This is an implementation of a stack data structure using arrays
  * A stack is LIFO.
  */
-public class Stack {
-	public int top;
-	public int myStack[];
-	public Stack(){ //constructor
-		top = -1;
-		myStack = new int[100];
+public class Stack<T> {
+	public int size;
+	public T myStack[];
+	@SuppressWarnings("unchecked")
+	public Stack(){
+		size = 0;
+		myStack = (T[])new Object[100];
 	}
 
-	public void push(int data){ //adds element to the stack
-		if(top + 1 == myStack.length){ //I could create a method to allocate larger array
-			System.out.println("Stack overflow!");
-			return;
+	public void push(T data){
+		if(size + 1 == myStack.length){ //I could create a method to allocate larger array
+			resize(2*myStack.length);
 		}
-		top++;
-		myStack[top] = data;
+		myStack[size++] = data;
 	}
-	public void pop(){ //delete element from the stack
-		if(top == -1){
-			System.out.println("Stack is empty!  Unable to pop!");
-			return;
+	public T pop(){
+		if(isEmpty() == true){
+			throw new NoSuchElementException("Stack underflow");
 		}
-		top--;
+		T data = myStack[size-1];
+		myStack[size-1] = null;
+		size--;
+		//Shrink size of array if necessary
+		if(size > 0 && size == myStack.length/4){
+			resize(myStack.length/2);
+		}
+		return data;
 	}
-	public int top(){ //Returns the value on top of the stack
-		
-		return myStack[top];
+	
+	public T peek(){
+		if(isEmpty() == true){
+			throw new NoSuchElementException("Stack underflow");
+		}
+		return myStack[size-1];
 	}
-	public boolean isEmpty(){ //tests if stack is empty
-		if(top == -1) {
+	
+	public boolean isEmpty(){
+		if(size == 0) {
 			return true;
 		}
 		return false;
 	}
-	public void printStack() { //prints the stack
-		for(int i = 0; i <= top; i++) {
+	public void printStack() {
+		for(int i = 0; i < size; i++) {
 			System.out.print(myStack[i] + " ");
 		}
 		System.out.println();
+	}
+	public int size(){
+		return size;
+	}
+	public void resize(int newSize){
+		@SuppressWarnings("unchecked")
+		T[] temp = (T[]) new Object[newSize];
+		for(int i = 0; i < size; i++){
+			temp[i] = myStack[i];
+		}
+		myStack = temp;
 	}
 }
