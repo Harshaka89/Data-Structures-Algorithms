ebc4c2d06d9628abcb29b7a09b93f799a8e2ee9e
Aaron Covington <TheAaronCovington@gmail.com>
2015-09-13 18:08:50 -0500

    Update StackLinkedList.java

--- Java/Stacks/StackLinkedList.java
+++ Java/Stacks/StackLinkedList.java
@@ -2,31 +2,65 @@
  * Implementation of a stack using a Linked List
  * A stack is LIFO
  */
-public class StackLinkedList {
-	public Node top; //head in normal linked list
-	public int size = 0;
-	
-	public void push(int data){
-		Node pushNode = new Node(data);
-		pushNode.next = top; //set next node to current top
-		top = pushNode; //set new top to the new node
-		size++; //increment size of list
+public class StackLinkedList<T> {
+	public class Node<T>{ //Node subclass
+		T data;
+		Node next;
+		Node(T data){
+			this.data = data;
+		}
 	}
-	public void pop(){
-		if(top == null){
+	//Instance Variables
+	private int size;
+	Node top;
+	StackLinkedList(){ //Constructor
+		size = 0;
+		top = null;
+	}
+	//Adds item to the top of the stack
+	public void push(T data){
+		Node newNode = new Node(data);
+		newNode.next = top;
+		top = newNode;
+		size++;
+	}
+	//Removes item from the top of the stack
+	public T pop(){
+		if(isEmpty()){
+			return null;
+		}
+		T data = (T)top.data;
+		top = top.next;
+		size--;
+		return data;
+	}
+	//Retrieves the item on top of the stack
+	public T top(){
+		return (T)top.data;
+	}
+	//Prints the entire stack
+	public void printStack(){
+		Node current = top;
+		if(isEmpty()){
 			return;
 		}
-		top = top.next; //pop the top node
-		size--;
-	}
-	public int top(){ //returns data in top node
-		return top.data;
+		for(Node i = top; i != null; i = i.next){
+			System.out.print(i.data + " ");
+		}
+		System.out.println();
 	}
 	
-	public boolean isEmpty(){ //checks if list is empty
-		if(top == null){
+	/*
+	 * Methods to get the size of the stack and to check if it is empty.
+	 */
+	public Boolean isEmpty(){ //checks if stack is empty
+		if(size == 0){
+			System.out.println("Stack is empty!");
 			return true;
 		}
 		return false;
+	}
+	public int getSize(){ //returns the size of the stack
+		return size;
 	}
 }
