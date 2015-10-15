38a1837356f8bbe0b5eac53a89e145b14c0c3caa
Aaron Covington <TheAaronCovington@gmail.com>
2015-10-15 08:28:19 -0500

    Update 01_GameOfThrones.java

--- Java/HashTable/HackerRank/01_GameOfThrones.java
+++ Java/HashTable/HackerRank/01_GameOfThrones.java
@@ -30,31 +30,32 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in); int count = 0;
-        String ans;
-        String inputString = myScan.nextLine();
-        Map<Character, Integer> myMap = new HashMap<Character, Integer>();
+        String ans; //will store answer
+        String inputString = myScan.nextLine(); //take input
+        Map<Character, Integer> myMap = new HashMap<Character, Integer>(); //Create HashTable
+        /*Iterate over characters in string and add them to the table, keeping count of duplicates*/
         for(int i = 0; i < inputString.length(); i++){
-        	if(myMap.containsKey(inputString.charAt(i))){
+        	if(myMap.containsKey(inputString.charAt(i))){ //If duplicate increase count by 1
         		myMap.put(inputString.charAt(i), myMap.get(inputString.charAt(i)) + 1);
         	}
-        	else{
+        	else{ //Otherwise add to table and set count to 1
         		myMap.put(inputString.charAt(i), 1);
         	}
         }
-        Iterator itr = myMap.values().iterator();
-        while(itr.hasNext()){
-        	Integer test = (Integer)itr.next();
-        	if(test % 2 != 0){
-        		count++;
-        		if(count > 1){
+        Iterator itr = myMap.values().iterator(); //Create iterator
+        while(itr.hasNext()){ //Check if there is another value in table
+        	Integer test = (Integer)itr.next(); //store value returned by iterator as Integer
+        	if(test % 2 != 0){ //Check if not even
+        		count++; //if not even then increase count
+        		if(count > 1){ //if more than one odd value it cant be a palindrome
         			ans = "NO";
-                    System.out.println(ans);
-        			return;
+                    System.out.println(ans); //print NO
+        			return; //exit
         		}
         	}
         }
         ans = "YES";
-        System.out.println(ans);
+        System.out.println(ans); //if loop completes it must be true
         myScan.close();
     }
 }
