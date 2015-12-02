64bda711e5df73b6050cd7a47f79f8b2a3501821
Aaron Covington <TheAaronCovington@gmail.com>
2015-12-02 12:27:57 -0600

    Update pp7_ShakerSort.cpp

--- C/CSCI2683/pp7_ShakerSort.cpp
+++ C/CSCI2683/pp7_ShakerSort.cpp
@@ -1,7 +1,7 @@
 /*
    NAME: AARON COVINGTON
    CLASS: CSCI 2683
-   DATE: 11/25/2015
+   DATE: 12/02/2015
    PROJECT: Programming Project 7
    DESCRIPTION:  Implement Shaker Sort
 */
@@ -10,8 +10,9 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <time.h>
 using namespace std;
-
+//Shaker sort algorithm to sort the list
 void shaker_sort(int list[], int N){
 	int temp = 0;
 	for(int i = 1; i < N; i++){
@@ -32,13 +33,28 @@ void shaker_sort(int list[], int N){
 	}
 }
 
-int main(int argc, char *argv[]) {
-    int list[] = {8, 23, 4, 16, 42, 15};
-    int N = sizeof(list)/sizeof(int);
-    shaker_sort(list, N);
-	for(int i = 0; i < N; i++){
+//This method initializes the array with 50 random integers
+void set(int list[], int N){
+    int temp = 0;
+    srand(time(NULL));
+    for(int i = 0; i < N; i ++){
+        list[i] = rand() % 100000; //Random number between 0 and 100000
+    }
+}
+
+//This method loops through the list and prints each element.
+void print(int list[], int N){ 
+    for(int i = 0; i < N; i++){ 
 		cout<<list[i]<<" ";
 	}
+}
+
+int main(int argc, char *argv[]) {
+    const int N = 50;
+    int list[N];
+    set(list, N); //Initialize list
+    shaker_sort(list, N); //Sort list
+	print(list, N); //print results
 	cout<<endl;
     return 0;
 }
