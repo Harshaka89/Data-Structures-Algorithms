1b163e7e8548e096633101d251f56533cfe43a7e
Aaron Covington <TheAaronCovington@gmail.com>
2016-03-10 07:33:51 -0600

    Update GCD.java

--- Java/Math/GCD.java
+++ Java/Math/GCD.java
@@ -1,10 +1,10 @@
 public class GCD {
 	public static long gcd(long A, long B){
-		if(B == 0){
+		if(B == 0){ //If B== 0 then GCD is found so return A
 			return A;
 		}
-		long R = A % B;
-		return gcd(B, R);
+		long R = A % B; //Calculate the remainder of A/B
+		return gcd(B, R); //Return B to divide it by the remainder
 	}
 	public static void main(String[] args){
 		System.out.println(gcd(3918848, 1653264));
