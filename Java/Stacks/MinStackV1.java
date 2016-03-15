292b3d4a80e9f68d5ff57eb3a62cc08a483475cf
Aaron Covington <TheAaronCovington@gmail.com>
2016-03-15 08:38:18 -0500

    Create MinStackV1.java

---
+++ Java/Stacks/MinStackV1.java
@@ -0,0 +1,46 @@
+class MinStack {
+    public class Node{
+        int data;
+        Node next;
+        Node(int x){
+            this.data = x;
+        }
+    }
+    Node head; Node minHead;
+    int size, minSize;
+    public MinStack(){
+        size = 0;
+        minSize = 0;
+    }
+    public void push(int x) {
+        Node newNode = new Node(x);
+        newNode.next = head;
+        head = newNode;
+        size++;
+        if(minSize == 0 || x <= minHead.data){
+            newNode = new Node(x);
+            newNode.next = minHead;
+            minHead = newNode;
+            minSize++;
+        }
+    }
+
+    public void pop() {
+        if(size != 0){
+            if(head.data == minHead.data){
+                minHead = minHead.next;
+                minSize--;
+            }
+            head = head.next;
+            size--;
+        }
+    }
+
+    public int top() {
+        return head.data;
+    }
+
+    public int getMin() {
+        return minHead.data;
+    }
+}
