5c68a64ec3d9a471f306643ae75c5d2c1bd507d7
Aaron Covington <TheAaronCovington@gmail.com>
2016-03-22 22:39:18 -0500

    Update AllFactors.java

--- Java/Math/AllFactors.java
+++ Java/Math/AllFactors.java
@@ -1,6 +1,21 @@
 /* Description:  Write a program to return all the factors of a given number N in a list
 */
 
+//Most efficient
+public static ArrayList<Integer> allFactors(int N){
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	for(int i = 1; i*i <= N; i++){
+		if(N % i == 0){
+			list.add(i);
+			if(i != N/i){
+				list.add(N/i);
+			}
+		}
+	}
+	Collections.sort(list);
+	return list;
+}
+
 /*Not as efficient*/
 public static ArrayList<Integer> allFactors(int N){
 		ArrayList<Integer> list = new ArrayList<Integer>();
