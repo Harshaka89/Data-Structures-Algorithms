c50872ea1e55847aae0978a389b1be93c97d84cc
Aaron Covington <TheAaronCovington@gmail.com>
2016-10-17 23:33:11 -0500

    Update 01_aplusb.cs

--- CSharp/ITMO/01_aplusb.cs
+++ CSharp/ITMO/01_aplusb.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Numerics;
 using System.Collections;
+using System.IO;
 
-namespace TopCoder
+namespace ITMO
 {
     class Program
-    {
-        
+    { 
         static void Main(string[] args) {
-            //string text = System.IO.File.    
+            string text = File.ReadAllText("aplusb.in");
+            string[] nums = text.Split(' ');
+            int result = int.Parse(nums[0]) + int.Parse(nums[1]);
+            Console.WriteLine(result);
+            System.IO.File.WriteAllText("aplusb.out", result + "");
 
         }
     }
